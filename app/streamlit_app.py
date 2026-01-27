94f32f79312cbdf644eb7e497832aa4d40b95c4a
ythollet <yanis.thollet69@gmail.com>
2026-01-27 09:32:44 +0100

    Création du graphique du nombre de produits terminés par jour

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,7 +4,11 @@ import os # Pour lire les variables d'environnement
 import pandas as pd # Importer pandas pour la manipulation des données
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import altair as alt
 
+
+# La configuration de la page doit être la première commande Streamlit
+st.set_page_config(layout='wide')
 
 # Récupération des informations de connexion depuis les variables d'environnement
 DB_HOST = os.environ.get("DB_HOST", "db") # Le nom de service est 'db'
@@ -33,7 +37,6 @@ def func_query_db(
 
 def func_page1():
     try:
-
         st.header("Affichage des 100 premières lignes")
 
         query = "SELECT * FROM tblfinorder;"
@@ -72,24 +75,89 @@ def func_page1():
                     value = df['End'].count()
                 )
 
-                # Graphique : Nombre de produits terminés par jour
-                df['End'] = pd.to_datetime(df['End'])
-                
-                # On récupère la date actuelle
-                today = datetime.now()
+                def func_barchart_end_products_by_days(in_df):
+                    
+                    # On travaille sur une copie pour éviter de modifier le DF original
+                    in_df = in_df.copy()
 
-                # On récupère les 30 derniers jours
-                mask = (df['End'] > today-relativedelta(days = 30))
-                col_end = df['End'].loc[mask].reset_index(drop=True)
 
-                # On groupe par jour et on coumpte les produits terminés pour chaque jour
-                col_end_groupby_day = col_end.groupby(col_end.dt.date).count().rename("nb produits terminés").to_frame()
-                
-                # Affiche le barchart
-                st.bar_chart(
-                    col_end_groupby_day,
-                    y = 'nb produits terminés'
-                )
+                    # Graphique : Nombre de produits terminés par jour
+                    in_df['End'] = pd.to_datetime(in_df['End'])
+                    
+                    # On récupère la date actuelle
+                    today = datetime.now()
+
+                    # On récupère les 60 derniers jours
+                    mask = (in_df['End'] > today-relativedelta(days = 100))
+                    col_end = in_df['End'].loc[mask].reset_index(drop=True)
+
+                    # On groupe par jour et on coumpte les produits terminés pour chaque jour
+                    df_end_groupby_day = col_end.groupby(col_end.dt.date).count().rename("nb_produits_termines").to_frame().reset_index()
+
+                    # Barres du graphique
+                    barres = alt.Chart(df_end_groupby_day).mark_bar(
+                        size = 30,
+                        cornerRadius = 5
+                        ).encode(
+                            x = alt.X(
+                                'End:T',
+                                axis = alt.Axis(
+                                    format = '%d %b',
+                                    title = 'Date',
+                                    titleFontWeight = 'bold'
+                                    )
+                                ),
+                            y = alt.Y(
+                                'nb_produits_termines',
+                                axis = alt.Axis(
+                                    title = 'Nombre produits terminés',
+                                    titleFontWeight = 'bold'
+                                    )
+                                ),
+                            color = alt.Color(
+                                'nb_produits_termines:Q',
+                                scale = alt.Scale(
+                                    range = ["#D0A0F7", "#9139D8", "#7A06DA"]
+                            ),
+                            legend = None
+                        )                      
+                            ).properties(
+                                title = alt.TitleParams(
+                                    text = 'Nombre de produits terminés par jour',
+                                    anchor = 'middle',
+                                    fontSize = 30
+                                )
+                                
+                            )
+
+                    # Etiquettes de données
+                    etiquettes = barres.mark_text(
+                        # color = 'white',
+                        dy = -12,
+                        fontSize = 15
+                        ).encode(
+                            text = alt.Text(
+                                'nb_produits_termines', 
+                                format = '.0f',
+                                
+                                )
+                            )
+
+                    # Seuil objectif
+                    df_seuil = pd.DataFrame({'seuil': [4]})
+                    seuil = alt.Chart(df_seuil).mark_rule(
+                        color = 'chartreuse',
+                        strokeDash = [5, 8],
+                        strokeCap = 'round'
+                        ).encode(
+                            y = 'seuil'
+                        )
+
+                    barchart = barres + etiquettes + seuil
+
+                    st.altair_chart(barchart, use_container_width=True)
+
+                func_barchart_end_products_by_days(df)
             
             with col2:
 
