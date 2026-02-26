b1823909e363b36b53181ca3b2a881ab2e54853f
HoeschleY <yannickhoeschle@gmail.com>
2026-02-26 08:33:25 +0100

    Ajout page Qualite + Taux de rebut
    Creation de la page qualité, de la gauge taux de rebuts et ajout de plotly et matplotlib dans requirements

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from page_production_1.main_page_production_1 import func_page_production_1
 from page_production_2.main_page_production_2 import func_page_production_2
 from page_logistique_stock.main_page_logistique_stock import func_page_logistique_stock
-
+from page_qualite.main_page_qualite import func_page_qualite
 
 
 # La configuration de la page doit être la première commande Streamlit
@@ -12,7 +12,8 @@ st.set_page_config(layout='wide')
 pages = [
     st.Page(func_page_production_1, title = 'Page Production 1', icon = "📊"),
     st.Page(func_page_production_2, title = 'Page Production 2', icon = "📊"),
-    st.Page(func_page_logistique_stock, title = 'Page Logistique/Stock')
+    st.Page(func_page_logistique_stock, title = 'Page Logistique/Stock'),
+    st.Page(func_page_qualite, title = 'Page Qualité', icon = "📈")
 ]
 
 app = st.navigation(pages)
